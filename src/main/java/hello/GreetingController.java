9c213b5485d25b86017614a0ba725c1446cb4f1b
Eli-Skaronea <eli.skaronea@gmail.com>
2018-06-29 07:05:40 -0600

    Modified application to be a consumer of the topic 'greetings' from kafka. The logs of what it is consuming are located in the console output of the application.

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -4,8 +4,15 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
 
 @RestController
+@Component
+@Slf4j
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
@@ -16,4 +23,14 @@ public class GreetingController {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
+
+   
+    // @RequestMapping("/recent")
+    // @StreamListener(GreetingsStreams.INPUT)
+    // public void handleGreetings(@Payload Greeting greetings) {
+    //     System.out.println(greetings);
+
+    // }
+
+    
 }
